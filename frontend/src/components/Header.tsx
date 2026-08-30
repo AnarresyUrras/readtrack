4e1e2b0a356fd388c48e911cb6af48ff3173bfe2
AnarresyUrras <laura.aguin@gmail.com>
2026-08-30 09:54:01 +0200

    Initial React frontend setup

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <header>
+            <h1>ReadTrack</h1>
+            <p>My personal reading tracker</p>
+        </header>
+    );
+}
+
+export default Header;
