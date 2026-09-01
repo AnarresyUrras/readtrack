e5c77a6e993d85b30c4e0f99ae661b31c3f960e6
AnarresyUrras <laura.aguin@gmail.com>
2026-09-01 14:52:30 +0200

    feat: set up Alembic database migrations

--- backend/app/models/enums.py
+++ backend/app/models/enums.py
@@ -4,3 +4,9 @@ class ReadingStatus(str, Enum):
     to_read = "to_read"
     reading = "reading"
     finished = "finished"
+
+class AuthorGender(str, Enum):
+    male = "male"
+    female = "female"
+    non_binary = "non_binary"
+    unknown = "unknown"
